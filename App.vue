4c8fb43db4281bf948204bfbe4357ff3453b9656
ACherkas12 <55573379+ACherkas12@users.noreply.github.com>
2021-12-02 14:11:12 +0300

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <h2 class="title">Poetry is an echo, asking a shadow to dance.
+</h2>
+    <Poetry />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Poetry from './components/Poetry.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Poetry
+  }
+})
+</script>
+
+
---
+++ Poetry.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="poetry">
+      <div class="placeholder-form">
+        <div class="placeholder-container">
+          <input type="text" placeholder=" " />
+          <label>Enter the title of the poem</label>
+        </div>
+      </div>
+      <div class="poetry-lines" v-for="value in poetry" :key="value.poetry">
+        <div class="poetry-lines_author">
+          <h3 class="poetry-lines_title">
+            {{ value.title }}
+          </h3>
+        </div>
+        <div class="poetry-lines_lines" >
+         <p> {{ value.lines}} </p>
+      </div>
+    </div>
+    </div>
+
+
+</template>
+
+<script lang="ts">
+import axios from 'axios'
+
+export default {
+  name: 'Poetry',
+  data() {
+    return {
+      poetry: [],
+      errors: []
+    }
+  },
+  created() {
+    axios.get('https://poetrydb.org/title/Ozymandias/lines.json')
+    .then(responce => {
+      this.poetry = responce.data.lines[0]
+      console.log(responce)
+    })
+    .catch(e=> {
+      this.errors.push()
+    })
+  }
+}
+
+</script>
